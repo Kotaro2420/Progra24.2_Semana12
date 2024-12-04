beee0bf8daa80b1bfed96e1edd22c9ef2821e6fd
Kotaro2420 <kotaroespinoza24@gmail.com>
2024-12-04 09:36:15 -0500

    Lobby Create

---
+++ Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,47 @@
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private Button startButton;
+    [SerializeField] private TMP_InputField playerNameInputField;
+    [SerializeField] private string GameplayScene;
+
+    private void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+        startButton.onClick.AddListener(OnClicked);
+    }
+
+
+    private void OnClicked()
+    {
+        GameData.playerName = playerNameInputField.text;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        RoomOptions options = new RoomOptions();
+        options.IsOpen = true;
+        options.IsVisible = true;
+        options.MaxPlayers = 4;
+
+        PhotonNetwork.JoinOrCreateRoom("Room1", options, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            PhotonNetwork.LoadLevel("GameplayScene");
+        }
+
+    }
+}
